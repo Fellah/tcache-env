caf9d4c9acbfeef37f7086e718401d1872b9a3b0
Roman Krivetsky <DishWasherSurfer@gmail.com>
2016-04-04 10:38:23 +0300

    Remove fields from table.

---
+++ db-modify.sql
@@ -0,0 +1,19 @@
+ALTER TABLE cached_sletat_tours DROP COLUMN hotel_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN town_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN star_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN meal_name RESTRICT;
+--ALTER TABLE cached_sletat_tours DROP COLUMN room_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN htplace_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_hotel_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_town_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_star_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_meal_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_room_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_htplace_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_country_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_dptcity_name RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_currency_id RESTRICT;
+ALTER TABLE cached_sletat_tours DROP COLUMN original_currency_name RESTRICT;
+
+ALTER TABLE cached_sletat_tours ADD COLUMN room_id INTEGER;
+--ALTER TABLE cached_sletat_tours ADD COLUMN room_name VARCHAR(255);
